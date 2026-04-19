2173c3bb642c7b1aad1624c05f90788776587d61
MRBT7274 <marselltor@gmail.com>
2026-04-19 22:01:52 +0300

    feat, refactor: changed networking to bypass cors, added first altitude output

--- mars-mfd/src/App.tsx
+++ mars-mfd/src/App.tsx
@@ -1,8 +1,19 @@
+import { useEffect, useState } from "react"
 
 function App() {
 
+  const [datalink, setDatalink] = useState();
+
+  useEffect(() => {
+    fetch('/api/telemachus/datalink?alt=v.altitude&pe=o.PeA&ap=o.ApA')
+      .then(res => res.json())
+      .then(data => setDatalink(data))
+    
+  });
+
   return (
     <>
+      wawa {datalink?.alt}
     </>
   )
 }
