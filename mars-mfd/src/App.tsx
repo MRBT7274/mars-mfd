0940426d0c66a700aef2b68ffb7ee62b1871d421
MRBT7274 <marselltor@gmail.com>
2026-04-20 00:04:39 +0300

    refactor: removed all errors

--- mars-mfd/src/App.tsx
+++ mars-mfd/src/App.tsx
@@ -8,9 +8,17 @@ function App() {
     "apoapsis": number
   }
 
+  type camera = {
+    "id": number,
+    "name": string,
+    "streaming": boolean,
+    "snapshotUrl": string,
+    "streamUrl": string,
+  }
+
   const [datalink, setDatalink] = useState<downlinked>();
 
-  const [cameras, setCameras] = useState<Array<object>>();
+  const [cameras, setCameras] = useState<Array<camera>>();
 
   const [counter, setCounter] = useState(0);
 
@@ -19,6 +27,10 @@ function App() {
       .then(res => res.json())
       .then(data => setCameras(data))
   }, [])
+
+  useEffect(() => {
+    console.log(cameras);
+  }, [cameras])
 
   useEffect(() => {
     fetch('/avionics/telemachus/datalink?altitude=v.altitude&periapsis=o.PeA&apoapsis=o.ApA')
