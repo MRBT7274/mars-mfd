a4ff4c6f784b60d2ee2a469b8e1cd1291c618439
MRBT7274 <marselltor@gmail.com>
2026-04-23 23:55:18 +0300

    hotfix: minor bug squashingification

--- mars-mfd/src/components/Header.tsx
+++ mars-mfd/src/components/Header.tsx
@@ -12,7 +12,6 @@ function Header() {
     }
 
     const [datalink, setDatalink] = useState<downlinked>();
-    const [missionTimeFormatted, setMissionTimeFormatted] = useState<Date>();
 
     const [counter, setCounter] = useState<string>();
 
