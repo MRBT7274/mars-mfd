74428c6c218a9c61f36378c69227f1a6504d0646
MRBT7274 <marselltor@gmail.com>
2026-04-24 13:13:28 +0300

    style: made standby prettier

--- mars-mfd/src/components/Standby.tsx
+++ mars-mfd/src/components/Standby.tsx
@@ -1,15 +1,22 @@
 function Standby() {
     return(
         <>
-            <div className="box-body" style={{alignItems: "center"}}>
-                <div>
-                    Marsell Aerospace Institute
-                </div>
-                <div>
-                    on standby
-                </div>
-                <div>
-                    mars-mfd v0.1
+            <div className="box-body" style={{alignItems: "center", color: "light"}}>
+                <div style={{
+                    borderWidth: "1.5px", borderStyle: "dashed", borderColor: "gray", borderRadius: "10px",
+                    padding: "1rem"
+                }}>
+                    <div style={{color: "lightgray"}}>
+                        <div>
+                            Marsell Aerospace Institute
+                        </div>
+                        <div>
+                            - on standby -
+                        </div>
+                        <div>
+                            mars-mfd v0.1
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
