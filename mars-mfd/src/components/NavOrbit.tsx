f135da6adb0b0ca32cda31e1f5de6dddbb589678
MRBT7274 <marselltor@gmail.com>
2026-04-26 21:26:03 +0300

    feat: created query generator to get name id pairs of all bodies

--- mars-mfd/src/components/NavOrbit.tsx
+++ mars-mfd/src/components/NavOrbit.tsx
@@ -5,14 +5,47 @@ import './OrbitStyles.css'
 function NavOrbit() {
 
     type downlinkedOrbit = {
-            "apoapsis": number,
-            "periapsis": number,
-            "refbody": string
-        }
+        "apoapsis": number,
+        "periapsis": number,
+        "refbody": string,
+        "sit": string
+    }
+
+    type bodyCount = {
+        "bodynum": number;
+    }
+
+    type body = {
+        "id": number;
+        "name": string;
+    }
     
     const [datalink, setDatalink] = useState<downlinkedOrbit>();
 
     const [counter, setCounter] = useState<string>();
+
+    const [totalBodies, setTotalBodies] = useState<bodyCount>();
+
+    //let bodyList = {}
+
+    useEffect(()=>{
+        fetch('/avcs/telemachus/datalink?bodynum=b.number')
+            .then(res => res.json())
+            .catch(() => {})
+            .then((data) => setTotalBodies(data))
+    },[])
+
+    useEffect(()=>{
+        if(totalBodies != undefined) {
+            let bodyRequest = "/avcs/telemachus/datalink?"
+            for(let i = 0; i < totalBodies?.bodynum; i++) {
+                bodyRequest = bodyRequest + "body" + i + "=b.name[" + i + "]&" 
+            }
+            console.log(bodyRequest)
+
+            
+        }
+    },[totalBodies])
 
     useEffect(()=>{
         const intervalSlow = setInterval(() => {
@@ -27,43 +60,21 @@ function NavOrbit() {
             '/avcs/telemachus/datalink?' +
             'refbody=o.referenceBody&' +
             'apoapsis=o.ApA&' +
-            'periapsis=o.PeA'
+            'periapsis=o.PeA&' +
+            'sit=v.situation&'
             
         )
             .then(res => res.json())
             .catch(() => {})
             .then(data => setDatalink(data))
+        
 
     }, [counter]);
 
-    const planetoid = {
-        position: "absolute",
-        top: "60%",
-        left: "50%",
-        transform: "translate(-50%, -50%)",
-        width: "100px",
-        height: "100px",
-        backgroundColor: "black",
-        borderColor: "green",
-        borderWidth: "2px",
-        borderStyle: "solid",
-        borderRadius: "50%"
-    } as unknown as undefined
-
-    const ellipse = {
-        position: "absolute",
-        top: "60%",
-        left: "50%",
-        transform: "translate(-50%, -50%)",
-        width: "200px",
-        height: "200px",
-        backgroundColor: "white",
-        clipPath: "ellipse(50% 30%)",
-    } as unknown as undefined
 
     return(
         <>
-            <div className="box-body">
+            <div className="box-body" style={{justifyContent: "flex-start"}}>
                 {/* <div style={{width: "200px", height: "100px", backgroundColor: "white", borderColor: "white",
                     clipPath: "ellipse(50% 30%)", borderWidth: "1px", borderStyle: "solid"
                 }}>
@@ -72,10 +83,51 @@ function NavOrbit() {
                     }}></div>
 
                 </div> */}
-                <div style={ellipse}></div>
-                <div style={planetoid}>
+                <div style={{textAlign: "left"}}>
+                    <p>ApA: {datalink?.apoapsis == undefined? "N/A" : datalink.apoapsis}</p>
+                    <p>PeA: {datalink?.periapsis == undefined? "N/A" : datalink.periapsis}</p>
+                    <p>{datalink?.sit}</p>
+                    <p>{totalBodies == undefined ? "N/A" : totalBodies.bodynum}</p>
+                </div>
+                
+
+                <div style={{
+                    position: "absolute",
+                    top: "60%",
+                    left: "50%",
+                    transform: "translate(-50%, -50%)",
+                    width: "200px",
+                    height: "200px",
+                    backgroundColor: "white",
+                    clipPath: "ellipse(50% 30%)"
+                }}></div>
+
+                {/* <div style={{
+                    position: "absolute",
+                    top: "60%",
+                    left: "50%",
+                    transform: "translate(-50%, -50%)",
+                    width: "199px",
+                    height: "199px",
+                    backgroundColor: "black",
+                    clipPath: "ellipse(50% 30%)"
+                }}></div> */}
+
+
+                <div style={{position: "absolute",
+                    top: "60%",
+                    left: "50%",
+                    transform: "translate(-50%, -50%)",
+                    width: "100px",
+                    height: "100px",
+                    backgroundColor: "black",
+                    borderColor: "green",
+                    borderWidth: "2px",
+                    borderStyle: "solid",
+                    borderRadius: "50%"
+                }}>
                     <div>
-                        <p className="refBody">{datalink?.refbody}</p>
+                        <p className="refBody">{datalink?.refbody == undefined? "N/A" : datalink.refbody}</p>
                         <p>*</p>
                     </div>
                 </div>
