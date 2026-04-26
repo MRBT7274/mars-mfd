c30f3dc48d49de6a56e1a13058cfb7b4c592636a
MRBT7274 <marselltor@gmail.com>
2026-04-26 17:55:50 +0300

    feat: added orbit display page with a planet indicator in the middle

--- mars-mfd/src/components/NavOrbit.tsx
+++ mars-mfd/src/components/NavOrbit.tsx
@@ -1,8 +1,39 @@
+
 function NavOrbit() {
+
+    const planetoid = {
+        position: "absolute",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+        width: "100px",
+        height: "100px",
+        backgroundColor: "black",
+        borderColor: "green",
+        borderWidth: "2px",
+        borderStyle: "solid",
+        borderRadius: "50%"
+    } as unknown as undefined
+
+    const ellipse = {
+        clipPath: "ellipse(50% 30%)"
+    } as unknown as undefined
 
     return(
         <>
-            navOrbit
+            <div className="box-body">
+                {/* <div style={{width: "200px", height: "100px", backgroundColor: "white", borderColor: "white",
+                    clipPath: "ellipse(50% 30%)", borderWidth: "1px", borderStyle: "solid"
+                }}>
+                    <div style={{width: "200px", height: "100px", backgroundColor: "black",
+                        clipPath: "ellipse(50% 30%)"
+                    }}></div>
+
+                </div> */}
+                <div style={planetoid}></div>
+                <div style={ellipse}></div>
+
+            </div>
         </>
     )
 }
