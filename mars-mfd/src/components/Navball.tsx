24b4aa879161b3aae1c1ffd9b8d5a1668cc1e944
MRBT7274 <marselltor@gmail.com>
2026-05-09 21:04:06 +0300

    feat: added attitude indicator basic design

--- mars-mfd/src/components/Navball.tsx
+++ mars-mfd/src/components/Navball.tsx
@@ -36,10 +36,63 @@ function Navball() {
     return(
         <>
             <div className="box-body">
-                <div style={{textAlign: "left", marginLeft: "1rem"}}>
-                    <p>{datalink?.heading}</p>
-                    <p>{datalink?.pitch}</p>
-                    <p>{datalink?.roll}</p>
+
+                <div style={{
+                    textAlign: "center",
+                    position: "absolute",
+                    top: "20%",
+                    left: "35vw",
+                    transform: "translate(-0%, -0%)",
+                    transformOrigin: "center",
+                    rotate: datalink?.roll + "deg"
+                }}>
+                    <div style={{position: "relative", overflowY: "clip", transform: "translate(0%, "+ datalink?.pitch +"%)"}}>
+                        <div style={{display: "flex", flexDirection: "row", justifyContent: "space-between"}}>
+                            <div style={{display: "flex", flexDirection: "column-reverse", textAlign: "left"}}>
+                                <br /><br /><p>-</p>
+                                <br /><br /><p>- 30</p>
+                                <br /><br /><p>-</p>
+                                <br /><br /><p>- 60</p>
+                                <br /><br /><p>-</p>
+                                <br /><br /><p>- 90</p>
+                            </div>
+
+                            <div style={{display: "flex", flexDirection: "column-reverse", textAlign: "right"}}>
+                                <br /><br /><p>-</p>
+                                <br /><br /><p>30 -</p>
+                                <br /><br /><p>-</p>
+                                <br /><br /><p>60 -</p>
+                                <br /><br /><p>-</p>
+                                <br /><br /><p>90 -</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <hr style={{width: "30vw"}} />
+
+                    
+
+                    <div style={{position: "relative", overflowY: "clip", transform: "translate(0%, "+ datalink?.pitch +"%)"}}>
+                        <div style={{display: "flex", flexDirection: "row", justifyContent: "space-between"}}>
+                            <div style={{display: "flex", flexDirection: "column", textAlign: "left"}}>
+                                <br /><br /><p>-</p>
+                                <br /><br /><p>- 30</p>
+                                <br /><br /><p>-</p>
+                                <br /><br /><p>- 60</p>
+                                <br /><br /><p>-</p>
+                                <br /><br /><p>- 90</p>
+                            </div>
+
+                            <div style={{display: "flex", flexDirection: "column", textAlign: "right"}}>
+                                <br /><br /><p>-</p>
+                                <br /><br /><p>30 -</p>
+                                <br /><br /><p>-</p>
+                                <br /><br /><p>60 -</p>
+                                <br /><br /><p>-</p>
+                                <br /><br /><p>90 -</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
