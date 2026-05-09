8e92d8f0bd3ec527bc6729a39e0cd76024669986
MRBT7274 <marselltor@gmail.com>
2026-05-09 22:29:31 +0300

    style: made attitude indicator look prettier and actually useful

--- mars-mfd/src/components/Navball.tsx
+++ mars-mfd/src/components/Navball.tsx
@@ -40,25 +40,35 @@ function Navball() {
                 <div style={{
                     textAlign: "center",
                     position: "absolute",
-                    top: "20%",
+                    top: window.innerHeight / 2 + "px",
                     left: "35vw",
                     transform: "translate(-0%, -0%)",
+                    margin: -window.innerWidth / 20 - window.innerHeight / 3 + "px 0%",
                     transformOrigin: "center",
                     rotate: datalink?.roll + "deg"
                 }}>
-                    <div style={{position: "relative", overflowY: "clip", transform: "translate(0%, "+ datalink?.pitch +"%)"}}>
+                    <div style={{position: "relative", overflowY: "clip", transform: "translate(0%, "+ datalink?.pitch +"%)", color: "cyan"}}>
                         <div style={{display: "flex", flexDirection: "row", justifyContent: "space-between"}}>
                             <div style={{display: "flex", flexDirection: "column-reverse", textAlign: "left"}}>
-                                <br /><br /><p>-</p>
+                                <br /><p>-</p>
                                 <br /><br /><p>- 30</p>
                                 <br /><br /><p>-</p>
                                 <br /><br /><p>- 60</p>
                                 <br /><br /><p>-</p>
-                                <br /><br /><p>- 90</p>
+                                <br /><br /><div>- 90</div>
                             </div>
 
                             <div style={{display: "flex", flexDirection: "column-reverse", textAlign: "right"}}>
-                                <br /><br /><p>-</p>
+                                <br /><br />
+                                <br /><br /><br />
+                                <br /><br /><br />
+                                <br /><br /><br />
+                                <br /><br /><br />
+                                <br /><br /><p>▷ ◁</p>
+                            </div>
+
+                            <div style={{display: "flex", flexDirection: "column-reverse", textAlign: "right"}}>
+                                <br /><p>-</p>
                                 <br /><br /><p>30 -</p>
                                 <br /><br /><p>-</p>
                                 <br /><br /><p>60 -</p>
@@ -68,14 +78,20 @@ function Navball() {
                         </div>
                     </div>
 
+                    <div style={{
+                        borderColor: "gray", borderWidth: "2px", borderStyle: "solid", width: "6.5rem"
+                    }}>{datalink?.roll == undefined? "N/A" : "roll: " + Math.floor(datalink.roll) + " ↻"}</div>
+
                     <hr style={{width: "30vw"}} />
 
-                    
+                    <div style={{
+                        borderColor: "gray", borderWidth: "2px", borderStyle: "solid", width: "6.5rem"
+                    }}>{datalink?.pitch == undefined? "N/A" : "pitch: " + Math.floor(datalink.pitch) + " ⇕"}</div>
 
-                    <div style={{position: "relative", overflowY: "clip", transform: "translate(0%, "+ datalink?.pitch +"%)"}}>
+                    <div style={{position: "relative", overflowY: "clip", transform: "translate(0%, "+ datalink?.pitch +"%)", color: "chocolate"}}>
                         <div style={{display: "flex", flexDirection: "row", justifyContent: "space-between"}}>
                             <div style={{display: "flex", flexDirection: "column", textAlign: "left"}}>
-                                <br /><br /><p>-</p>
+                                <br /><p>-</p>
                                 <br /><br /><p>- 30</p>
                                 <br /><br /><p>-</p>
                                 <br /><br /><p>- 60</p>
@@ -83,8 +99,28 @@ function Navball() {
                                 <br /><br /><p>- 90</p>
                             </div>
 
+                            <div style={{display: "flex", flexDirection: "column", textAlign: "right", width: "55%"}}>
+                                <div style={{display: "flex", flexDirection: "row", justifyContent: "space-around"}}>
+                                    <div>/</div> <div>/</div> <div>/</div> <div>/</div> <div>/</div>
+                                    <div>/</div> <div>/</div> <div>/</div> <div>/</div> <div>/</div>
+                                </div>
+                                <div style={{display: "flex", flexDirection: "row", justifyContent: "space-around"}}>
+                                    <div>/</div> <div>/</div> <div>/</div> <div>/</div> <div>/</div>
+                                    <div>/</div> <div>/</div> <div>/</div> <div>/</div> <div>/</div>
+                                </div>
+                                <div style={{display: "flex", flexDirection: "row", justifyContent: "space-around"}}>
+                                    <div>/</div> <div>/</div> <div>/</div> <div>/</div> <div>/</div>
+                                    <div>/</div> <div>/</div> <div>/</div> <div>/</div> <div>/</div>
+                                </div>
+                                <br /><br />
+                                <br /><br /><br />
+                                <br /><br /><br />
+                                <br /><br /><br />
+                                <br /><br /><p style={{display: "flex", justifyContent: "space-around"}}>X</p>
+                            </div>
+
                             <div style={{display: "flex", flexDirection: "column", textAlign: "right"}}>
-                                <br /><br /><p>-</p>
+                                <br /><p>-</p>
                                 <br /><br /><p>30 -</p>
                                 <br /><br /><p>-</p>
                                 <br /><br /><p>60 -</p>
